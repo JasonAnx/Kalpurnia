37d373f90d32bbdb14e427c0aa9910946f3cefde
Jason <jsonanx@localhost.localdomain>
2016-09-30 23:24:13 -0600

    extraer hrefs

--- webSearch/webSearch/spiders/CalpurniaCrawler_spider.py
+++ webSearch/webSearch/spiders/CalpurniaCrawler_spider.py
@@ -1,8 +1,12 @@
+from urllib2 import urlopen
 import scrapy
+from scrapy.selector import Selector
+from scrapy.http import HtmlResponse
 
 
 class QuotesSpider(scrapy.Spider):
     name = "CalpurniaCrawler"
+    
     start_urls = [
         'https://en.wikipedia.org/wiki/Calpurnia_(wife_of_Caesar)',
     ]
@@ -14,9 +18,25 @@ class QuotesSpider(scrapy.Spider):
             'content':content.css('p *::text').re(r'\w+')
         }
         
-        for caca in content.css('div'):
+        yield {
+            'content':content.css('a[href]::text')
+        }
+        
+        yield {
+            'content':response.css('a').xpath('@href').extract()
+        }
+        
+        body = '<html><body><span>good</span></body></html>'
+        response = HtmlResponse(url='https://en.wikipedia.org/wiki/Calpurnia_(wife_of_Caesar)', body=body)
+        d = Selector(response=response).xpath('//span/text()').extract()
+        
+        print (d) 
+        
+        for cnt in content.css('div'):
             yield {
                 # p parrafo, y todo lo que tenga dentro
                 
                 #'content': quote.css('ul *::text').re(r'\w+'),
             }
+    
+    
