442ea19dacb0710190af7aeade95c12275c47ce7
Jason <jsonanx@localhost.localdomain>
2016-10-02 13:07:58 -0600

    pasar Doc como argumento para parsear

--- webSearch/webSearch/spiders/CalpurniaCrawler_spider.py
+++ webSearch/webSearch/spiders/CalpurniaCrawler_spider.py
@@ -1,3 +1,4 @@
+from scrapy.spider import BaseSpider
 from urllib2 import urlopen
 import scrapy
 import os
@@ -8,43 +9,61 @@ from stemming.porter2 import stem
 
 class QuotesSpider(scrapy.Spider):
     name = "CalpurniaCrawler"
+    allowed_domains = ["wiki.archlinux.org"]
+    start_urls = ['https://wiki.archlinux.org/']
     
     
     #os.remove("*.json")
-    #print("File Removed!")
+    #print("File Removed!")s
 
     start_urls = [
         'https://wiki.archlinux.org/',
     ]
+
+    def __init__(self, **kw):
+        print("sadfasdf")
+        return
     
     
     def parse(self, response):
+        
+        #yield {
+        #    'content':response.css('a').xpath('@href').extract()
+        #}
+
+        #yield {
+        #        'url': response.url
+        #    };
+#
+#
+#
+        #hrefs = response.css('a').xpath('@href').extract()
+        #for ref in hrefs:
+        #    yield {
+        #        'hrefs': ref 
+        #    };
+
+        next_page = "https://wiki.archlinux.org/"
+        yield scrapy.Request(next_page, callback = self.tokenizeDoc)
+
+
+        #next_page = response.css('li.next a::attr(href)').extract_first()
+        #if next_page is not None:
+        #    next_page = response.urljoin(next_page)
+        #    yield scrapy.Request(next_page, callback = self.parse)
+        return
+
+    def tokenizeDoc (self, response):
         content = response.css('#content')
+
+        #"https://www.archlinux.org/"
 
         for word in content.css('p *::text').re(r'\w+'):
             yield {
                 'content': stem( word )
             }
         
-        yield {
-            'hrefs':content.css('a[href]::text')
-        }
-        
-        yield {
-            'content':response.css('a').xpath('@href').extract()
-        }
-        
-        body = '<html><body><span>good</span></body></html>'
-        response = HtmlResponse(url='https://en.wikipedia.org/wiki/Calpurnia_(wife_of_Caesar)', body=body)
-        d = Selector(response=response).xpath('//span/text()').extract()
-        
-        print (d) 
-        
-        for cnt in content.css('div'):
-            yield {
-                # p parrafo, y todo lo que tenga dentro
-                
-                #'content': quote.css('ul *::text').re(r'\w+'),
-            }
-    
-    
+        #yield {
+        #    'hrefs':content.css('a[href]::text')
+        #}
+        return
